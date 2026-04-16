3bd6da71b1088d89b908ecfd725df3e24776c784
rj45c <zhuwei_net@163.com>
2026-04-16 22:29:19 +0800

    test

--- posts-list.js
+++
@@ -1,7 +0,0 @@
-export const postsList = [
-    { file: "test.md" },
-    { file: "boke1.md" },
-    { file: "ceshitu.md" },
-    { file: "新手必看：Markdown（MD）语法全面使用指南.md" },
-    // 新文章只需要加一行：{ file: "新文章.md" }
-]
